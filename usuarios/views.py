8af8692ea80eef14f61fcb27bd5ec40205a38f6a
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2024-09-02 23:30:53 -0300

    fix

--- usuarios/views.py
+++ usuarios/views.py
@@ -66,3 +66,9 @@ def cadastro(request):
 
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso!!")
+    return redirect('login')
